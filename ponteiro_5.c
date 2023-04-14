e66cc5dfd128e8abed9f460015882491d46af8fd
Eli Santos Reseda <50843398+Ereseda@users.noreply.github.com>
2023-04-14 08:14:01 -0300

    Add files via upload

---
+++ ponteiro_5.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+//https://www.youtube.com/watch?v=FD0bi4H08IU
+
+struct x{
+    int a;
+    int b;
+    int c;
+};
+int main(void){
+   // int x;
+    //double y;
+    //int vetor[10];
+    struct x estrutura;
+    printf("%li", sizeof(estrutura));
+    return 0;
+}
